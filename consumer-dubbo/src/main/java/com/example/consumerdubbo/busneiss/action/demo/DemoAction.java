73839eb377620bdd20756ec8608d4fd64060208f
liqaing <18679117141@163.com>
2019-09-12 15:03:51 +0800

    修改注释

--- consumer-dubbo/src/main/java/com/example/consumerdubbo/busneiss/action/demo/DemoAction.java
+++ consumer-dubbo/src/main/java/com/example/consumerdubbo/busneiss/action/demo/DemoAction.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-//修改
 @RestController
 public class DemoAction {
     @Reference(version = "1.0.0", timeout = 300)
