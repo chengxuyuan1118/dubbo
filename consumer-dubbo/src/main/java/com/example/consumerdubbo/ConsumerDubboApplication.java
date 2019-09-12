a23a5cbb0b49b03fa88c2f0a9b4f9828fd2e6835
liqaing <18679117141@163.com>
2019-09-12 15:08:51 +0800

    修改注释

--- consumer-dubbo/src/main/java/com/example/consumerdubbo/ConsumerDubboApplication.java
+++ consumer-dubbo/src/main/java/com/example/consumerdubbo/ConsumerDubboApplication.java
@@ -3,6 +3,7 @@ package com.example.consumerdubbo;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @EnableDubboConfiguration
 @SpringBootApplication
 public class ConsumerDubboApplication {
